f0254cd77497d420d64a805852ebd64856a32bcb
Glen Seeber <gseeber5@gmail.com>
2022-02-26 19:26:47 -0800

    Create several funcs for operations We made a separate func for the topic pages and for popular texts. created some functions which we might use often within other functions, like dictMaker() and fetchResults() honestly this should've been like, at least 5 separate commits but here we are :/

--- main.py
+++ main.py
@@ -2,38 +2,76 @@ from bs4 import BeautifulSoup
 import requests
 
 #funcs
+
+#general purpose
 def convert(string, breaker):
     li = list(string.split(breaker))
     return li
 
-#get the link to the amazon search page which you want to scrape
-f = open('link.txt', 'r')
-myLink = f.read()
-f.close()
-
-#pull the page
-myPage = requests.get(myLink).content
-
-soup = BeautifulSoup(myPage, 'lxml')
-
-
-f = open('myPage.html', 'w')
-f.write(soup.prettify())
-f.close()
+#makes a dict from the structure (the keys) and a label var (the values)
+# both should be lists. the label will have info like author name, book title, etc.
+def dictMaker(structure, label):
+    output = {}
+    for i in range(len(label)):
+        try:
+            output.update({structure[0] : label[0]})
+        # in case of the label having more data than the structure can account
+        # for, just use "other" as the key
+        except:
+            output.update({"other" : label[i]})
 
 
-def popTexts(soup):
-    results = soup.find(id="widepage").find(class_="list-group").find_all(class_="amw-listing-item")
-    print(results)
-
+# takes a list of results (use find_all()) and iterates through, and seperates
+# results into a list: [Book Title, Author name, (whatever else may be tagged on at the end)]
+def fetchResults(results, breaker):
+    output = []
     for item in results:
         # splits the item into a list, ['Book Title', 'Author']
         label = convert(item.text, ' — ')
         #                    remove newline characters
         book = label[0].replace('\n', '')
         # split 'author + pages' into list (use 6 spaces as split) 
-        secondHalf = convert(label[1], '      ')
+        secondHalf = convert(label[1].replace('  ', ''), breaker)
         #author is the first half of that list
         author = secondHalf[0]
-        print(f"Book Title: {book}\nAuthor Name: {author}\n")
 
+        #takes book title, author, and all list items formed after that, and puts
+        # them into a single, non-nested, list.
+        # ex: [book title, author name, date posted, pp number]
+        thisLabel = [book]
+        for i in secondHalf:
+            if (i != ''):
+                thisLabel.append(i)
+        #debug - print statement is temporary
+        print(f"Book Title: {book}\nAuthor Name: {author}\n")
+        # add a nested dict of this specific label to the list, which we will output once
+        # we have iterated through all labels.
+        output.append(dictMaker(thisLabel))
+    return output
+
+        
+
+def popTexts(soup):
+    results = soup.find(id="widepage").find(class_="list-group").find_all(class_="amw-listing-item")
+    data = fetchResults(results, '      ')
+    return results
+
+def topicPage(soup):
+    results = soup.find(id="widepage").find(class_="amw-post-listing-container").div.div.find_all(class_="amw-listing-item")
+    #feed fetchResults your find_all() list of items, it will give you the 
+    output = fetchResults(results, '\n')
+    return output
+
+#endregion
+
+#the page to scrape
+myLink = "https://theanarchistlibrary.org/category/topic/black-flag"
+
+
+#pull the page
+myPage = requests.get(myLink).content
+
+soup = BeautifulSoup(myPage, 'lxml')
+
+#
+topicPage(soup)
